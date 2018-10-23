5da9cf9a8f1373b687ef747f8eee78024b579eed
joris <joris@2le.net>
2018-10-23 11:37:09 +0200

    init

---
+++ src/LleAttachmentBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lle\AttachmentBundle;
+
+use Lle\EasyAdminPlusBundle\Acl\Compiler\TwigPathPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LleAttachmentBundle extends Bundle
+{
+}
