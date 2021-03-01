46c9a9e980488182c13b950a0e9febd82dea81f7
joris <joris@2le.net>
2021-03-01 11:05:07 +0100

    Update ThumbAction.php

--- src/Action/ThumbAction.php
+++ src/Action/ThumbAction.php
@@ -5,7 +5,7 @@ namespace Lle\AttachmentBundle\Action;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\File\MimeType\FileinfoMimeTypeGuesser;
+use Symfony\Component\Mime\FileinfoMimeTypeGuesser;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Lle\AttachmentBundle\Service\AttachmentManager;
 
@@ -25,7 +25,7 @@ final class ThumbAction{
             $path = $this->manager->getAbsolutePath($file);
             $response = new BinaryFileResponse($path);
             $mimeTypeGuesser = new FileinfoMimeTypeGuesser();
-            $response->headers->set('Content-Type', ($mimeTypeGuesser->isSupported()) ? $mimeTypeGuesser->guess($path) : 'text/plain');
+            $response->headers->set('Content-Type', ($mimeTypeGuesser->isGuesserSupported()) ? $mimeTypeGuesser->guessMimeType($path) : 'text/plain');
             $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, $file->getFilename());
         }else{
             if($this->manager->getIconByMimeType($file->getMimetype())){
